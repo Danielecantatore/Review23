b5d1592c30d7463365b30e6daa9d02777d585141
danielecantatore <d.cantatore8@studenti.uniba.it>
2018-01-03 10:35:10 +0100

    first commit

---
+++ class/MyException.php
@@ -0,0 +1,36 @@
+<?php
+
+class FileException extends Exception { 
+    
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }   
+}
+
+class InvalidFileNameException extends Exception { 
+    
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }   
+}
+
+class InvalidParException extends Exception { 
+    
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }   
+}
+
+class NullParException extends Exception { 
+    
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }   
+}
+
+class InvalidArgException extends Exception { 
+    
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }   
+}
